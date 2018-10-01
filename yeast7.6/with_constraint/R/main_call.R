f7bb48cdbf87264bb443fb928730e05c91a43bfe
deyazoubi <43732248+deyazoubi@users.noreply.github.com>
2018-10-01 18:07:07 +0200

    Add files via upload

---
+++ yeast7.6/with_constraint/R/main_call.R
@@ -0,0 +1,13 @@
+
+path<-"Data_scripts_models/yeast7.6/with_constraint/R/"
+
+# yeast with constraint
+
+source(paste0(path,'yeast_preprocesing.R'))# done
+source(paste0(path,'yeast_with_constraint'))# done
+
+
+ 
+# yeast with constraint currency metabolice
+source(paste0(path,'yeast_preprocesing.R'))# done
+source(paste0(path,'PL_FreeMets_yeast_with_constraint.R'))# done
